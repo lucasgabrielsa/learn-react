ca9216bf7b108c5b38f372151d381ac27c0cb866
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-05-19 16:48:55 -0300

    Better version to conditional render

--- src/components/login/ButtonLogin.js
+++ src/components/login/ButtonLogin.js
@@ -1,29 +1,6 @@
 import React from "react";
 
 function ButtonLogin(props) {
-  //let butttonToRender;
-  // if (!props.isLogged) {
-  //   butttonToRender = (
-  //     <button
-  //       type="button"
-  //       className="btn btn-primary"
-  //       onClick={() => props.loginHandle()}
-  //     >
-  //       Log in
-  //     </button>
-  //   );
-  // } else {
-  //   butttonToRender = (
-  //     <button
-  //       type="button"
-  //       className="btn btn-secondary"
-  //       onClick={() => props.loginHandle()}
-  //     >
-  //       Loggout
-  //     </button>
-  //   );
-  // }
-
   const butttonToRender = (
     <button
       type="button"
