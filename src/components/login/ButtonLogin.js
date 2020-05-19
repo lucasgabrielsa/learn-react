df7fb5290938b18a316c5da091c472e1b6162f36
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-05-19 10:51:39 -0300

    Conditional Rendering

--- src/components/login/ButtonLogin.js
+++ src/components/login/ButtonLogin.js
@@ -1,28 +1,38 @@
 import React from "react";
 
 function ButtonLogin(props) {
-  let butttonToRender;
-  if (!props.isLogged) {
-    butttonToRender = (
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => props.loginHandle()}
-      >
-        Log in
-      </button>
-    );
-  } else {
-    butttonToRender = (
-      <button
-        type="button"
-        className="btn btn-secondary"
-        onClick={() => props.loginHandle()}
-      >
-        Loggout
-      </button>
-    );
-  }
+  //let butttonToRender;
+  // if (!props.isLogged) {
+  //   butttonToRender = (
+  //     <button
+  //       type="button"
+  //       className="btn btn-primary"
+  //       onClick={() => props.loginHandle()}
+  //     >
+  //       Log in
+  //     </button>
+  //   );
+  // } else {
+  //   butttonToRender = (
+  //     <button
+  //       type="button"
+  //       className="btn btn-secondary"
+  //       onClick={() => props.loginHandle()}
+  //     >
+  //       Loggout
+  //     </button>
+  //   );
+  // }
+
+  const butttonToRender = (
+    <button
+      type="button"
+      className={`btn btn-${props.isLogged ? "secondary" : "primary"}`}
+      onClick={() => props.loginHandle()}
+    >
+      {props.isLogged ? "Loggout" : "Log in"}
+    </button>
+  );
 
   return <div>{butttonToRender}</div>;
 }
